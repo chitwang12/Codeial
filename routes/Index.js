0b1db2b1dd0a6a4238cece83c31ab381c10301c6
chitwang12 <chitwangandhi12@gmail.com>
2023-02-01 14:09:48 +0530

    Setting up View Engine

--- routes/Index.js
+++ routes/Index.js
@@ -6,5 +6,11 @@ const homeController = require('../Controllers/home_controller');
 console.log("Router loaded");
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+
+
+// for any further routes , access from here
+// router .use ('/routerName' , require('./routerfile'));
 
 module.exports = router;
