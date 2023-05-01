f11bd061923529cc9be14bce9c983a25cb2c3414
chitwang12 <chitwangandhi12@gmail.com>
2023-05-02 00:43:24 +0530

    Set up the directory structure and many more basic things

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 8000;
+
+app.listen(PORT,function(err){
+    if(err)
+    {
+        console.log(`Error in running the application : ${err} `);
+    }
+    console.log(`Server is running successfully on port : ${PORT}`)
+})
