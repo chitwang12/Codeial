b8aa4910c8d1b3c2ef3ccb21f8c3c33f605cb93c
chitwang12 <chitwangandhi12@gmail.com>
2023-05-02 11:46:15 +0530

    home Controller set up has been done

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.port || 8000;
 
+
+//Use Express Router 
+app.use('/', require('./routes'));
+
+
 app.listen(PORT,function(err){
     if(err)
     {
