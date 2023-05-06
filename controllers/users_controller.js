a3e6ac4c7053b12c8483c8294905225a912095bf
chitwang12 <chitwangandhi12@gmail.com>
2023-05-07 00:43:03 +0530

    Manual Authentication Sign up was done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/users');
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
         title:'User Profile'
@@ -19,11 +20,28 @@ module.exports.signIn = function(req,res){
 }
 
 //get the sign up data
-module.exports.create = function(req,res){
-    //To DO later 
+module.exports.create = async function(req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            const newUser = await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log('Error in creating user while signing up', err);
+        return res.redirect('back');
+    }
 }
+
+   
 
 //Sign in and create a session for the user 
 module.exports.createSession = function(req,res){
-    //To DO later 
+   
 }
