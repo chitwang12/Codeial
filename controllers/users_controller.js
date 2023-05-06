6b26a006fd1db305a9257082fb7ed4b469bdaebc
chitwang12 <chitwangandhi12@gmail.com>
2023-05-06 19:13:27 +0530

    Users Controller with sign in and sign up routes has been set up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile!</j1>')
+    return res.render('user_profile',{
+        title:'User Profile'
+    })
+}
+
+//render the sign up page 
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:'Codeial | Sign Up'
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:'Codeial | Sign In'
+    })
 }
