593d379b60e1ec5cc37d5ff127b03f00dcf20930
chitwang12 <chitwangandhi12@gmail.com>
2023-05-09 00:30:37 +0530

    Posts Controller and routes for creating posts is setup and now working fine!!!

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,27 @@
-module.exports.home = function(req,res){
-   return res.render('home',{
-    title : "Home"
+const Post = require('../models/post');
+
+module.exports.home = async function(req,res){
+//    try {
+//       const posts = await Post.find({});
+//       return res.render('home', {
+//          title: "Codieal | Home",
+//          posts: posts
+//       });
+//    } catch (err) {
+//       console.log('Error in fetching posts from database', err);
+//       return;
+//    }
+// }
+
+//Doing this because we automatically populating the user while working in posts db 
+try {
+   const posts = await Post.find({}).populate('user').exec();
+   return res.render('home', {
+     title: 'Codeial | Home',
+     posts: posts,
    });
+ } catch (err) {
+   console.log('Error in fetching posts from database', err);
+   return res.redirect('back');
+ }
 }
