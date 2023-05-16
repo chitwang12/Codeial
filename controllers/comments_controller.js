056e01d6f8cfdeacc792c96d78e8de97d1518d1d
chitwang12 <chitwangandhi12@gmail.com>
2023-05-16 16:37:01 +0530

    Deleting the posts and comments feature implemented

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,24 @@ module.exports.create = async function(req, res) {
       // handle error
     }
   }
+
+  module.exports.destroy = async function(req,res){
+    try{
+      const comment = await Comment.findById(req.params.id);
+
+      if(comment.user == req.user.id){
+        let postId = comment.post ;
+       await comment.deleteOne();
+       await Post.findByIdAndUpdate(postId,{$pull :{comments:req.params.id}});
+          return res.redirect('back');
+        }
+        else{
+        return res.redirect('back');
+      }
+    }
+    catch(err)
+    {
+      console.log('Error in deleting Comment : ',err);
+      return res.redirect('back');
+    }
+  };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comments');
 module.exports.create = async function(req, res) {
     try {
       const post = await Post.create({
@@ -9,6 +10,24 @@ module.exports.create = async function(req, res) {
     } catch (err) {
       console.log('Error in creating a post', err);
       return;
+    }
+  };
+
+  module.exports.destroy = async function(req, res) {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      if (post.user == req.user.id) {
+        await post.deleteOne();
+        await Comment.deleteMany({ post: req.params.id });
+        
+        return res.redirect('back');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log('Error in deleting post: ', err);
+      return res.redirect('back');
     }
   };
   
